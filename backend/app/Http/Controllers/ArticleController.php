e59df8bf6723456567eca17d81161358740d3b20
yushi265 <ae265.1108@gmail.com>
2021-04-07 22:33:46 +0900

    5-5

--- backend/app/Http/Controllers/ArticleController.php
+++ backend/app/Http/Controllers/ArticleController.php
@@ -9,6 +9,11 @@ use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
+    public function __construct()
+    {
+        $this->authorizeResource(Article::class, 'article');
+    }
+
     public function index()
     {
         $articles = Article::all()->sortByDesc('created_at');
@@ -44,5 +49,10 @@ class ArticleController extends Controller
     {
         $article->delete();
         return redirect()->route('articles.index');
+    }
+
+    public function show(Article $article)
+    {
+        return view('articles.show', ['article' => $article]);
     }
 }
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -14,3 +14,4 @@
 Auth::routes();
 Route::get('/', 'ArticleController@index')->name('articles.index');
 Route::resource('/articles', 'ArticleController')->except(['index'])->middleware('auth');
+Route::resource('/articles', 'ArticleController')->only(['show']);
