f0b765b36053b507169b4ffc7e590c1c2014edd5
yushi265 <ae265.1108@gmail.com>
2021-04-10 07:23:58 +0900

    6-5

--- backend/resources/views/articles/card.blade.php
+++ backend/resources/views/articles/card.blade.php
@@ -67,7 +67,9 @@
 
     <div class="card-body pt-0 pb-2 pl-3">
         <div class="card-text">
-            <article-like></article-like>
+            <article-like :initial-is-liked-by='@json($article->isLikedBy(Auth::user()))'>
+
+            </article-like>
         </div>
     </div>
 </div>
