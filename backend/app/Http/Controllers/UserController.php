888a12babcf08a2c013de1abfe09e60c8f5756e0
yushi265 <ae265.1108@gmail.com>
2021-04-14 21:35:45 +0900

    9-2

---
+++ backend/app/Http/Controllers/UserController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function show(string $name)
+    {
+        $user = User::where('name', $name)->first();
+
+        return view('users.show', ['user' => $user]);
+    }
+}
