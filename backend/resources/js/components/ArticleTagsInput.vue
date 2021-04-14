05bc4d6a45c4c0e71aac2b77b0deac64cd880b7b
yushi265 <ae265.1108@gmail.com>
2021-04-14 20:29:29 +0900

    8-11

--- backend/resources/js/components/ArticleTagsInput.vue
+++ backend/resources/js/components/ArticleTagsInput.vue
@@ -26,29 +26,16 @@ export default {
       initialTags: {
           type: Array,
           default: [],
+      },
+      autocompleteItems: {
+          type: Array,
+          default: [],
       }
   },
   data() {
     return {
       tag: "",
       tags: this.initialTags,
-      autocompleteItems: [
-        {
-          text: "Spain",
-        },
-        {
-          text: "France",
-        },
-        {
-          text: "USA",
-        },
-        {
-          text: "Germany",
-        },
-        {
-          text: "China",
-        },
-      ],
     };
   },
   computed: {
@@ -76,5 +63,8 @@ export default {
   margin-right: 4px;
   border-radius: 0px;
   font-size: 13px;
+}
+.vue-tags-input .ti-tag::before {
+    content: "#";
 }
 </style>
